98bb86d67ce1e83620e9e69e0bc8955f5824652b
Victor Pinheiro Mello <victormello7003@hotmail.com>
2025-03-26 18:33:25 -0300

    Criando-NavBar

---
+++ app/componets/navbar/NavBar.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+{
+  /* import Image from "next/image"; */
+}
+import Link from "next/link";
+
+const Navbar = (): React.ReactNode => {
+  return (
+    <nav className="w-full rounded-3xl flex justify-around !p-4 bg-zinc-900">
+      <div className="flex shrink-0 gap-1">
+        {/* <Image src="" width={100} height={100} alt="Logo" /> */}
+        <h1 className="text-white font-black">MG Ambiental</h1>
+      </div>
+
+      <ul className="flex gap-3 text-white">
+        <li className="text-emerald-600 font-bold hover:text-emerald-600">
+          <Link href="">Inicio</Link>
+        </li>
+        <li className="font-bold hover:text-emerald-600">
+          <Link href="">Sobre nós</Link>
+        </li>
+        <li className="font-bold hover:text-emerald-600">
+          <Link href="">Laboratório de Análises Ambientais</Link>
+        </li>
+        <li className="font-bold hover:text-emerald-600">
+          <Link href="">Engenharia Ambiental</Link>
+        </li>
+        <li className="font-bold hover:text-emerald-600">
+          <Link href="">Entre em Contato</Link>
+        </li>
+        <li className="font-bold hover:text-emerald-600">
+          <Link href="">Parceiros</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
