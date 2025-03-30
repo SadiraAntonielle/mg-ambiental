349a6afcb5dcb836ae6ee9bf1825ce50d9c2c664
Victor Pinheiro Mello <victormello7003@hotmail.com>
2025-03-30 19:34:55 -0300

    home

--- app/page.tsx
+++ app/page.tsx
@@ -1,97 +1,62 @@
+import React from "react";
 import Image from "next/image";
-import styles from "./page.module.css";
+import Link from "next/link";
+
 import Navbar from "./componets/navbar/NavBar";
 
-export default function Home() {
+export default function Home(): React.ReactNode {
   return (
-    <div className={styles.page}>
-      <Navbar />
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <>
+      <header className="w-screen p-8">
+        <Navbar />
+      </header>
+      <main className="flex gap-3 w-screen h-screen max-h-2/3 py-8 px-16 items-center">
+        <section className="max-w-1/2 w-full p-2">
+          <h1 className="font-bold text-8xl">Ma Ambiental</h1>
+          <h3 className="text-3xl">
+            Bem-vindo à maior empresa de consultoria ambiental do Sul de Minas
+            Gerais.
+          </h3>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
+          <nav className="flex justify-center gap-3 mt-10">
+            <Link
+              className="bg-emerald-800 p-2 rounded-2xl font-black hover:bg-emerald-900 text-center"
+              href="*"
+            >
+              Laboratório de Análises Ambientais
+            </Link>
+            <Link
+              className="bg-emerald-800 p-2 rounded-2xl font-black hover:bg-emerald-900 text-center"
+              href="*"
+            >
+              Engenharia Ambiental
+            </Link>
+            <Link
+              className="bg-emerald-800 p-2 rounded-2xl font-black hover:bg-emerald-900 text-center"
+              href="*"
+            >
+              Topografia e Georreferenciamento
+            </Link>
+          </nav>
+        </section>
+
+        <aside className="max-w-1/2 w-full h-full grid grid-cols-2 p-2">
+          <div className="border border-amber-50 justify-self-end self-end">
+            teste 1
+          </div>
+          <div className="border border-amber-50 justify-self-stretch self-center">
+            teste 2
+          </div>
+          <div className="border border-amber-50">teste 3</div>
+          <div className="border border-amber-50 justify-self-start self-start">
+            teste 4
+          </div>
+          <div className="col-start-2 border border-amber-50 justify-self-stretch self-center">
+            teste 5
+          </div>
+        </aside>
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      <footer></footer>
+    </>
   );
 }
