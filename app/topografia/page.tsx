b3d7abe574ef2057855ff0ff83930cc0d211fe8b
Victor Pinheiro Mello <victormello7003@hotmail.com>
2025-04-03 15:50:16 -0300

    modificando-imagens

--- app/topografia/page.tsx
+++ app/topografia/page.tsx
@@ -14,7 +14,7 @@ export default function Topografia(): React.ReactNode {
       <main className="flex gap-3 w-screen h-screen max-h-8/10  px-16 items-center">
         <section className="flex flex-col gap-2 justify-around items-center bg-[#97E1A1] h-full w-1/2 rounded-4xl p-8">
           <Image
-            src="/lab01.png"
+            src="/topgrafia.png"
             alt=""
             width={1000}
             height={1000}
@@ -33,7 +33,9 @@ export default function Topografia(): React.ReactNode {
 
         <aside className="flex flex-col gap-4 justify-around items-center bg-emerald-900 h-full w-1/2 rounded-4xl p-8">
           <div className="text-[#9CE2A4] font-bold text-base">
-            <h3>Dentre os trabalhos que desenvolvemos citamos.</h3>
+            <h3 className="text-center">
+              Dentre os trabalhos que desenvolvemos citamos.
+            </h3>
             <p>
               Georreferenciamento de imóvel rural com certificação junto ao
               INCRA e registro no Cartório de Registro de Imóveis
@@ -59,7 +61,7 @@ export default function Topografia(): React.ReactNode {
             </p>
           </div>
           <Image
-            src="/lab02.png"
+            src="/top02.jpg"
             alt=""
             width={1000}
             height={1000}
